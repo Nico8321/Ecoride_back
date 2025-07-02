063eb7815dc46396c8cfd11f64dc893c6520b41e
Nico8321 <189213788+Nico8321@users.noreply.github.com>
2025-07-02 11:20:22 +0200

    feat: contrôleur Utilisateur (signup, signin, patch, password)

---
+++ controllers/utilisateurController.php
@@ -0,0 +1,80 @@
+<?php
+require_once __DIR__ . '/models/utilisateur.php';
+require_once __DIR__ . '/config/database.php';
+
+class UtilisateurController
+{
+    private $pdo;
+
+    public function __construct()    //connection a la bdd
+    {
+        $this->pdo = Database::getConnection();
+    }
+
+    public function getPdo()
+    {
+        return $this->pdo;
+    }
+
+    public function getOne($id) //recuperation d'un utilisateur par l'id 
+    {
+        $utilisateur = Utilisateur::findUtilisateurById($this->pdo, $id);
+        if ($utilisateur) {
+            unset($utilisateur['password']);
+        }
+        echo json_encode($utilisateur);
+    }
+
+    public function signUp($data)
+    {
+        if (Utilisateur::emailExists($this->pdo, $data['email'])) {
+            http_response_code(409);
+            echo json_encode(["error" => "Email déjà utilisé"]);
+            return;
+        }
+
+        $utilisateur = new Utilisateur($data);
+        $utilisateur->save($this->pdo);
+        echo json_encode(["message" => "Utilisateur créé"]);
+    }
+
+    public function signIn($data)
+    {
+        $utilisateur = Utilisateur::findUtilisateurByMail($this->pdo, $data['email']);
+
+        if ($utilisateur && password_verify($data['password'], $utilisateur['password'])) {
+            unset($utilisateur['password']);
+            echo json_encode($utilisateur);
+        } else {
+            http_response_code(401);
+            echo json_encode(["error" => "Identifiants invalides"]);
+        }
+    }
+
+    public function patchUser($id, $data)
+    {
+        $success = Utilisateur::updateUtilisateur($this->pdo, $id, $data);
+
+        if ($success) {
+            echo json_encode(["message" => "Utilisateur modifié"]);
+        } else {
+            http_response_code(404);
+            echo json_encode(["error" => "Utilisateur non trouvé ou non modifié"]);
+        }
+    }
+    public function patchUserPassword($id, $data)
+    {
+        $utilisateur = Utilisateur::findUtilisateurById($this->pdo, $id);
+        if ($utilisateur && password_verify($data['password'], $utilisateur['password'])) {
+            $hashedPassword = password_hash($data['newPassword'], PASSWORD_BCRYPT);
+            $success = Utilisateur::updateUtilisateurPassword($this->pdo, $id, $hashedPassword);
+
+            if ($success) {
+                echo json_encode(["message" => "Mot de passe modifié"]);
+            } else {
+                http_response_code(404);
+                echo json_encode(["error" => "Utilisateur non trouvé ou non modifié"]);
+            }
+        }
+    }
+}
