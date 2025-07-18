641d0ea1741ce06b7e4878dab3788e9947654d2e
Nico8321 <189213788+Nico8321@users.noreply.github.com>
2025-07-18 15:11:36 +0200

    US10 - Ajout des infos conducteur et véhicule dans les réservations + fix route index.php

--- controllers/reservationController.php
+++ controllers/reservationController.php
@@ -3,6 +3,9 @@ require_once __DIR__ . "/../models/reservation.php";
 require_once __DIR__ . "/../config/database.php";
 require_once __DIR__ . '/../models/covoiturage.php';
 require_once __DIR__ . '/../utils/securisationSortie.php';
+require_once __DIR__ . '/../models/utilisateur.php';
+require_once __DIR__ . '/../models/vehicule.php';
+require_once __DIR__ . '/../models/avis.php';
 
 class ReservationController
 {
@@ -37,6 +40,35 @@ class ReservationController
             http_response_code(404);
             echo json_encode(["error" => "Aucune réservation trouvée"]);
         } else {
+            foreach ($reservations as &$reservation) {
+                $covoiturageId = $reservation['covoiturage_id'];
+                $covoiturage = Covoiturage::findCovoiturageById($this->pdo, $covoiturageId);
+
+                if ($covoiturage) {
+                    // Enrichir avec conducteur
+                    $conducteur = Utilisateur::findUtilisateurById($this->pdo, $covoiturage['conducteur_id']);
+                    if ($conducteur) {
+                        $covoiturage['conducteur_photo'] = $conducteur['photo'] ?? null;
+                        $covoiturage['conducteur_pseudo'] = $conducteur['pseudo'];
+                        $avis = Avis::getMoyenneByUtilisateurId($this->pdo, $covoiturage['conducteur_id']);
+                        $covoiturage['conducteur_note'] = $avis;
+                        // $covoiturage['conducteur_note'] = $conducteur['note'];
+                    }
+
+                    // Enrichir avec véhicule
+                    $vehicule = Vehicule::findById($this->pdo, $covoiturage['vehicule_id']);
+                    if ($vehicule) {
+                        $covoiturage['vehicule_marque'] = $vehicule['marque'];
+                        $covoiturage['vehicule_modele'] = $vehicule['modele'];
+                        $covoiturage['vehicule_couleur'] = $vehicule['couleur'];
+                        $covoiturage['vehicule_energie'] = $vehicule['energie'];
+                    }
+
+                    // Ajouter le covoiturage enrichi à la réservation
+                    $reservation['covoiturage'] = $covoiturage;
+                }
+            }
+
             echo json_encode(securisationSortie($reservations));
         }
     }
