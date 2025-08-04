5c0255921f4d70629490f4dcd82c6aedd494bac6
Nicolas Beuve <nicolasbeuve@MacBook-Pro.local>
2025-08-04 09:32:50 +0200

    mise en place de phpMailer pour la notification par email (acceptation/refus de réservation, annulation covoiturage, feedback) - en cours de dev

---
+++ utils/models_mail/mail_acceptation.php
@@ -0,0 +1,2 @@
+<h1>Bonjour <?= $pseudo ?>,</h1>
+<p>Votre réservation pour le covoiturage du <?= $d_depart ?> a été acceptée par <?= $conducteur_pseudo ?>.</p>
---
+++ utils/models_mail/mail_annulation.php
@@ -0,0 +1,2 @@
+<h1>Bonjour <?= $pseudo ?>,</h1>
+<p> <?= $conducteur_pseudo ?>a annulé son covoiturage du <?= $d_depart ?> votre réservation pour le covoiturage est donc annulée.</p>
