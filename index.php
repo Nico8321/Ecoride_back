ea915b3240ba8f3e72b200f21755cc64c503e037
Nico8321 <189213788+Nico8321@users.noreply.github.com>
2025-08-05 14:56:03 +0200

    maj index.php pour preprod

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '');
-$dotenv->load();
+if (file_exists(__DIR__)) {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+}
 // CORS
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT,PATCH, DELETE, OPTIONS');
