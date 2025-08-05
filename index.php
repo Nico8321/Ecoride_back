d21e9894d68de45f2ad482b874aea58d5f27df53
Nico8321 <189213788+Nico8321@users.noreply.github.com>
2025-08-05 16:31:52 +0200

    chore(back): modifs CORS et front-controller avec .htaccess

--- index.php
+++ index.php
@@ -1,26 +1,27 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
-if (file_exists(__DIR__)) {
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-    $dotenv->load();
-}
-// CORS
+//CORS
 header('Access-Control-Allow-Origin: https://ecoride-front-w7vl.vercel.app');
 header('Access-Control-Allow-Methods: GET, POST, PUT,PATCH, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 header('Content-Type: application/json');
 
+// Gérer les requêtes préflight CORS
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit;
 }
-// Gérer les requêtes préflight CORS
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit();
+
+if (file_exists(__DIR__ . '/.env')) {
+    Dotenv\Dotenv::createImmutable(__DIR__)->load();
 }
+
+
+
+
+
 
 require_once __DIR__ . '/config/database.php';
 
