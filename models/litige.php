00612246d7cdaeb5fa9162168da04f0b81df0cca
Nico8321 <189213788+Nico8321@users.noreply.github.com>
2025-08-29 11:02:26 +0200

    feat(us12): ajout modèle Litige pour gestion et suivi des litiges

--- models/litige.php
+++ models/litige.php
@@ -9,34 +9,103 @@ use MongoDB\Collection;
 class Litige
 {
 
+    private static function oid(string $id): ObjectId
+    {
+        try {
+            return new ObjectId($id);
+        } catch (\Exception $e) {
+            throw new \InvalidArgumentException("Id invalide : $id");
+        }
+    }
+
+    //Creer Litige
+
     public static function create(Collection $col, $data)
     {
         if (empty($data['message'])) {
             throw new \InvalidArgumentException('Message obligatoire.');
         };
-        $now = new UTCDateTime();
+
         $res = $col->insertOne([
-            'reservation_id' => $data['reservation_id'],
-            'utilisateur_id' => $data['utilisateur_id'],
+            'reservation' => [
+                'id' => $data['reservation_id'],
+                'nb_places' => $data['nb_places']
+            ],
+            'redacteur' => [
+                'id' => $data['utilisateur_id'],
+                'pseudo' => $data['utilisateur_pseudo'],
+                'mail' => $data['utilisateur_mail'],
+            ],
+            'conducteur' => [
+                'id' => $data['conducteur_id'],
+                'pseudo' => $data['conducteur_pseudo'],
+                'mail' => $data['conducteur_mail'],
+            ],
+            'covoiturage' => [
+                'date_depart' => $data['date_depart'],
+                'prix' => $data['prix'],
+                'ville_depart' => $data['ville_depart'],
+            ],
             'message' => $data['message'],
-            'status' => 'ouvert',
-            'createdAt' => $now
+            'status'    => 'en_attente',
+            'suivi'     => [],
+            'createdAt' => new UTCDateTime(),
+            'updatedAt' => new UTCDateTime(),
         ]);
         return $res->getInsertedId();
     }
+
+    //Recuperer litige par l'id
+
     public static function getById(Collection $col, $id)
     {
 
-        $idLitige = new ObjectId($id);
-        $res = $col->findOne(['_id' => $idLitige]);
-        return $res;
+
+        return $col->findOne(['_id' => self::oid($id)]);
     }
-    public static function changeStatut(Collection $col, $id)
+
+    //Ajout d'une note de suivi au litige
+
+    public static function addNote(Collection $col, $id, $note)
     {
-        $idLitige = new ObjectId($id);
-        $res = $col->updateOne(
-            ['_id' => $idLitige],
-            ['$set' => ['status' => 'En cours de traitement']]
+        return $col->updateOne(
+            ['_id' => self::oid($id)],
+            [
+                '$push' => ['suivi' => [
+                    'note' => $note,
+                    'at'      => new UTCDateTime()
+                ]],
+                '$set'  => ['status' => 'en_traitement', 'updatedAt' => new UTCDateTime()]
+            ]
+        );
+    }
+
+    // Récuperation des litiges en attente 
+
+    public static function getLitigeEnAttente(Collection $col)
+    {
+        return $col->find(['status' => 'en_attente'], ['sort' => ['createdAt' => -1]]);
+    }
+    // Récuperation des litiges en traitement
+
+    public static function getLitigeEnTraitement(Collection $col)
+    {
+        return $col->find(['status' => 'en_traitement'], ['sort' => ['createdAt' => -1]]);
+    }
+
+    //Cloture d'un litige
+
+    public static function cloture(Collection $col, $id)
+    {
+        return $col->updateOne(
+            ['_id' => self::oid($id)],
+            [
+                '$set' => [
+                    'status'    => 'clos',
+                    'closedAt'  => new UTCDateTime(),
+                    'updatedAt' => new UTCDateTime()
+                ]
+            ]
         );
     }
 }
