58f02c8c46a5d94fd53325a751434d06797b79ad
Nico8321 <189213788+Nico8321@users.noreply.github.com>
2025-09-02 14:54:55 +0200

    feat(auth/avis): sécurisation des routes avis - ajout checkId() sur création d'avis - vérification auteur_id côté controller

--- routes/avisRoutes.php
+++ routes/avisRoutes.php
@@ -1,29 +1,42 @@
 <?php
 
 require_once __DIR__ . '/../controllers/avisController.php';
-require_once __DIR__ . '/../utils/requireRoleId.php';
+require_once __DIR__ . '/../utils/requireAuth.php';
 
 $controller = new AvisController();
 $method = $_SERVER['REQUEST_METHOD'];
 
 header('Content-Type: application/json');
-
+//recuperation des avis d'un utilisateur
 if ($method === 'GET' && $uri[0] === 'avis' && isset($_GET['utilisateur_id'])) {
     $controller->getAvisByUtilisateurId($_GET['utilisateur_id']);
-} elseif ($method === 'GET' && $uri[0] === 'avis-moyenne' && isset($_GET['moyenne_id'])) {
+}
+// recuperation moyenne conducteur 
+elseif ($method === 'GET' && $uri[0] === 'avis-moyenne' && isset($_GET['moyenne_id'])) {
     $controller->getMoyenneByUtilisateurId($_GET['moyenne_id']);
-} elseif ($method === 'GET' && $uri[0] === 'avis' && isset($_GET['covoiturage_id'])) {
+}
+// recuperation des avis par covoiturage
+elseif ($method === 'GET' && $uri[0] === 'avis' && isset($_GET['covoiturage_id'])) {
     $controller->getAvisByCovoiturageId($_GET['covoiturage_id']);
-} elseif ($method === 'POST' && $uri[0] === 'avis') {
+}
+// ajout d'un avis d'un passager
+elseif ($method === 'POST' && $uri[0] === 'avis' && isset($_GET['utilisateur_id'])) {
+    checkId($_GET['utilisateur_id']);
     $data = json_decode(file_get_contents('php://input'), true);
-    $controller->addAvis($data);
-} elseif ($method === 'GET' && isset($uri[0], $uri[1]) && $uri[0] === 'avis' && $uri[1] === 'staff') {
+    $controller->addAvis($data, $_GET['utilisateur_id']);
+}
+//recuperation des avis a valider
+elseif ($method === 'GET' && isset($uri[0], $uri[1]) && $uri[0] === 'avis' && $uri[1] === 'staff') {
     requireRole(2);
     $controller->getAllAvisToCheck();
-} elseif ($method === 'PATCH' && $uri[0] === 'avis' && isset($uri[1], $uri[2])  && $uri[2] === 'accepte') {
+}
+//acceptation d'un avis
+elseif ($method === 'PATCH' && $uri[0] === 'avis' && isset($uri[1], $uri[2])  && $uri[2] === 'accepte') {
     requireRole(2);
     $controller->validerAvis($uri[1]);
-} elseif ($method === 'PATCH' && $uri[0] === 'avis' && isset($uri[1], $uri[2]) && $uri[2] === 'refus') {
+}
+//refus d'un avis
+elseif ($method === 'PATCH' && $uri[0] === 'avis' && isset($uri[1], $uri[2]) && $uri[2] === 'refus') {
     requireRole(2);
     $controller->refusAvis($uri[1]);
 } else {
