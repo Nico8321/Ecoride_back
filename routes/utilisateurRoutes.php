750ce5f92864b0b72d81f88a3892d7bb561941d0
Nico8321 <189213788+Nico8321@users.noreply.github.com>
2025-09-02 14:00:25 +0200

    feat(auth/user,vehicule): sécurisation des routes véhicules et utilisateurs - ajout middleware checkId() sur routes sensibles

--- routes/utilisateurRoutes.php
+++ routes/utilisateurRoutes.php
@@ -1,32 +1,36 @@
 <?php
 require_once __DIR__ . '/../controllers/utilisateurController.php';
-
+require_once __DIR__ . '/../utils/requireAuth.php';
 
 $controller = new UtilisateurController();
 $method = $_SERVER['REQUEST_METHOD'];
 
 
 header('Content-Type: application/json');
-
+//creation compte
 if ($method === 'POST' && $uri[0] === 'user' && $uri[1] === 'signup') {
     $data = json_decode(file_get_contents("php://input"), true);
     $controller->signUp($data);
-}
+} //connection
 if ($method === 'POST' && $uri[0] === 'user' && $uri[1] === 'signin') {
     $data = json_decode(file_get_contents("php://input"), true);
     $controller->signIn($data);
-}
+} // patch infos utilisateur
 if ($method === 'PATCH' && $uri[0] === 'user' && isset($uri[1]) && !isset($uri[2])) {
+    checkId($uri[1]);
     $data = json_decode(file_get_contents("php://input"), true);
     $controller->patchUser($uri[1], $data);
-}
-if ($method === 'PATCH' && $uri[0] === 'user' && isset($uri[1]) && $uri[2] === "password") {
+} // patch password utilisateur
+if ($method === 'PATCH' && $uri[0] === 'user' && isset($uri[1], $uri[2]) && $uri[2] === "password") {
+    checkId($uri[1]);
     $data = json_decode(file_get_contents("php://input"), true);
     $controller->patchUserPassword($uri[1], $data);
-}
+} //suppression compte utilisateur
 if ($method === 'DELETE' && $uri[0] === 'user' && isset($uri[1]) && !isset($uri[2])) {
+    checkId($uri[1]);
     $controller->deleteUser($uri[1]);
-}
+} //ajout photo de profil utilisateur
 if ($method === 'POST' && $uri[0] === 'user' && $uri[1] === 'photo' && isset($uri[2])) {
+    checkId($uri[2]);
     $controller->addPhoto($uri[2]);
 }
--- routes/vehiculeRoutes.php
+++ routes/vehiculeRoutes.php
@@ -1,20 +1,23 @@
 <?php
 require_once __DIR__ . '/../controllers/vehiculeController.php';
+require_once __DIR__ . '/../utils/requireAuth.php';
 
 $controller = new VehiculeController();
 $method = $_SERVER['REQUEST_METHOD'];
 
 
 header('Content-Type: application/json');
-
+//ajout d'un vehicule
 if ($method === 'POST' && $uri[0] === "vehicule" && $uri[1] === 'user' && isset($uri[2])) {
+    checkId($uri[2]);
     $data = json_decode(file_get_contents("php://input"), true);
     $controller->addVehicule($data, $uri[2]);
-}
-if ($method === 'DELETE' && $uri[0] === 'vehicule' && isset($uri[1]) && $uri[2] === "user" && isset($uri[3])) {
-    $data = json_decode(file_get_contents("php://input"), true);
+} //supression d'un vehicule
+if ($method === 'DELETE' && $uri[0] === 'vehicule' && isset($uri[1], $uri[2], $uri[3]) && $uri[2] === "user") {
+    checkId($uri[3]);
     $controller->deleteVehicule($uri[1], $uri[3]);
-}
+} // récuperation des vehicules
 if ($method === 'GET' && $uri[0] === 'vehicule' && $uri[1] === 'user' && isset($uri[2])) {
+    checkId($uri[2]);
     $controller->getVehicule($uri[2]);
 }
